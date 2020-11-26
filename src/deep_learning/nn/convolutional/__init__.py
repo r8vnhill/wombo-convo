c18d955b2fcc58d8bb05f787e3c223a0e9adb8a6
SL8R <ignacio.slater@ug.uchile.cl>
2020-11-25 21:57:22 -0300

    Instructions (*Inception module*)

---
+++ src/deep_learning/nn/convolutional/__init__.py
@@ -0,0 +1,7 @@
+"""
+"wombo-convo" (c) by Ignacio Slater M.
+"wombo-convo" is licensed under a
+Creative Commons Attribution 4.0 International License.
+You should have received a copy of the license along with this
+work. If not, see <http://creativecommons.org/licenses/by/4.0/>.
+"""
